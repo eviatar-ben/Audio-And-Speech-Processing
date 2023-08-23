1c038c7d6393b7b1ae5304c9af9535eb3de25be7
eviatar <eviatar>
2023-08-23 17:07:30 +0300

    before pulling alons version to start work i=on inference

--- final project/ASR.py
+++ final project/ASR.py
@@ -4,6 +4,7 @@ import wandb
 
 import HyperParameters
 
+
 def init_w_and_b(hyper_params):
     epochs = hyper_params['epochs']
     learning_rate = hyper_params['learning_rate']
@@ -46,10 +47,23 @@ def run_model(hparams):
     TrainAndEvaluation.train_and_validation(hparams, all_iterators)
 
 
-if __name__ == '__main__':
+def test_model(hparams, model):
+    if HyperParameters.WB:
+        wandb.login()
+        init_w_and_b(hparams)
 
+    test_batch_iterator = DataLoader.get_batch_iterator("test", hparams["batch_size"],
+                                                        feat_type=hparams["feat_type"],
+                                                        n_feats=hparams["n_feats"])
+
+    TrainAndEvaluation.test(hparams, test_batch_iterator, model)
+
+
+if __name__ == '__main__':
     # run_model(HyperParameters.res_cnn_hparams)
     # run_model(HyperParameters.transformer_hparams)
     # run_model(HyperParameters.rnn_hparams)
     # run_model(HyperParameters.multiTransformer_hparams)
-    run_model(HyperParameters.deep_speech_hparams)
+    # run_model(HyperParameters.deep_speech_hparams)
+    # test_model(HyperParameters.res_cnn_hparams)
+    test_model()
--- final project/TrainAndEvaluation.py
+++ final project/TrainAndEvaluation.py
@@ -31,16 +31,16 @@ def train(model, device, batch_iterator, criterion, optimizer, scheduler, epoch)
 
         total_train_loss += loss.item()
 
-        if batch_idx % 50 == 0 or batch_idx == data_len-1:
+        if batch_idx % 50 == 0 or batch_idx == data_len - 1:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(spectrograms), data_len,
                        100. * batch_idx / data_len, loss.item()))
 
         # log to wandb once every epoch:
-        if batch_idx == data_len-1 and WB:
+        if batch_idx == data_len - 1 and WB:
             wandb.log({"train_loss": total_train_loss / data_len}, step=epoch)
             decoded_preds, decoded_targets = Utils.greedy_decoder(output.transpose(0, 1), labels,
-                                                                       label_lengths)
+                                                                  label_lengths)
             wer_sum = 0
             cer_sum = 0
             for j in range(len(decoded_preds)):
@@ -87,10 +87,9 @@ def validation(model, device, val_loader, criterion, epoch):
             print('{} -> {}'.format(decoded_targets[i], decoded_preds[i]))
 
     if WB:
-        wandb.log({"val_loss": val_loss},step=epoch)
+        wandb.log({"val_loss": val_loss}, step=epoch)
         wandb.log({"val_wer": avg_wer}, step=epoch)
         wandb.log({"val_cer": avg_cer}, step=epoch)
-
 
 
 def train_and_validation(hparams, batch_iterators):
@@ -114,3 +113,60 @@ def train_and_validation(hparams, batch_iterators):
     for epoch in range(1, epochs + 1):
         train(model, device, train_loader, criterion, optimizer, scheduler, epoch)
         validation(model, device, val_loader, criterion, epoch)
+
+
+def test_epoch(model, device, test_loader, criterion, epoch):
+    print('\nevaluating...')
+    model.eval()
+    test_loss = 0
+    test_wer = []
+    test_cer = []
+    with torch.no_grad():
+        for i, _data in enumerate(test_loader):
+            spectrograms, labels, input_lengths, label_lengths = _data
+            spectrograms, labels = spectrograms.to(device), labels.to(device)
+
+            output = model(spectrograms)  # (batch, time, n_class)
+            output = F.log_softmax(output, dim=2)
+            output = output.transpose(0, 1)  # (time, batch, n_class)
+
+            loss = criterion(output, labels, torch.from_numpy(input_lengths), torch.from_numpy(label_lengths))
+            test_loss += loss.item() / len(test_loader)
+
+            decoded_preds, decoded_targets = Utils.greedy_decoder(output.transpose(0, 1), labels, label_lengths)
+            for j in range(len(decoded_preds)):
+                test_wer.append(wer(decoded_targets[j], decoded_preds[j]))
+                test_cer.append(cer(decoded_targets[j], decoded_preds[j]))
+
+    avg_wer = sum(test_wer) / len(test_wer)
+    avg_cer = sum(test_cer) / len(test_cer)
+
+    print(
+        'test set: Average loss: {:.4f}, Average WER: {:.4f}\n'.format(test_loss, avg_wer))
+
+    # print a sample of the test data and decoded predictions against the true labels
+    if epoch % 10 == 0:
+        print('Ground Truth -> Decoded Prediction')
+        for i in range(10):
+            print('{} -> {}'.format(decoded_targets[i], decoded_preds[i]))
+
+    if WB:
+        wandb.log({"test_loss": test_loss}, step=epoch)
+        wandb.log({"test_wer": avg_wer}, step=epoch)
+        wandb.log({"test_cer": avg_cer}, step=epoch)
+
+
+def test(hparams, test_batch_iterator, model, save=True):
+    epochs = hparams['epochs']
+
+    torch.manual_seed(1)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    criterion = nn.CTCLoss(blank=28).to(device)
+
+    for epoch in range(1, epochs + 1):
+        # break
+        test_epoch(model, device, test_batch_iterator, criterion, epoch)
+
+    if save:
+        torch.save(model.state_dict(), 'data/model.pt')
